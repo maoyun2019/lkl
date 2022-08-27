2f4151b614f9b620a9d2e936ac54e52360edd5cf
fish4terrisa-MSDSM <63049888+fish4terrisa-MSDSM@users.noreply.github.com>
2022-08-27 20:14:28 +0800

    update boot.c

--- tools/lkl/tests/boot.c
+++ tools/lkl/tests/boot.c
@@ -543,7 +543,7 @@ static int lkl_test_execve(void)
 	} else {
 		lkl_test_logf("execve failed :( ret = %ld",ret);
 		lkl_test_logf("errno is %ld",errno);
-		fprintf("\nerrno :",strerror(errno));
+		fprintf("errno :",strerror(errno));
 		return TEST_FAILURE;
 	}
 }
